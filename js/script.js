4e9e4ae64c507ab51e652cbb16591fdf86d5b0c3
dadda68 <nils.birgersson@elev.ga.ntig.se>
2024-03-20 14:47:09 +0100

    Lektion 5

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-// Denna fil skall innehålla JavaScript (JS)
+function togglePopupOne() {
+    document.getElementById("popup-1").classList.toggle("active");
+}
+function togglePopupTwo() {
+    document.getElementById("popup-2").classList.toggle("active");
+}
+function togglePopupThree() {
+    document.getElementById("popup-3").classList.toggle("active");
+}
+function togglePopupFour() {
+    document.getElementById("popup-4").classList.toggle("active");
+}
+function togglePopupFive() {
+    document.getElementById("popup-5").classList.toggle("active");
+}
+function togglePopupSix() {
+    document.getElementById("popup-6").classList.toggle("active");
+}
